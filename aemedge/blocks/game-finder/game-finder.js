c8c85d8d2596610ab5cc4d0d090e286c1b382593
Kiran Murugulla <murugull@adobe.com>
2025-05-21 14:26:54 -0400

    sling props read logic is less error prone

--- aemedge/blocks/game-finder/game-finder.js
+++ aemedge/blocks/game-finder/game-finder.js
@@ -12,13 +12,27 @@ export default async function decorate(block) {
     matchupImgFormat: 'png',
   };
   const config = await readBlockConfig(block);
-  if (config.leagueList) {
-    config.leagueList = config.leagueList.split(',');
+  // Clean up config values
+  const cleanedConfig = {};
+  Object.entries(config).forEach(([key, value]) => {
+    if (typeof value === 'string') {
+      cleanedConfig[key] = value.trim() ? value : undefined;
+    } else if (typeof value === 'number') {
+      cleanedConfig[key] = !Number.isNaN(value) ? value : undefined;
+    } else if (typeof value === 'boolean') {
+      cleanedConfig[key] = value;
+    } else {
+      cleanedConfig[key] = value;
+    }
+  });
+  if (cleanedConfig.leagueList) {
+    cleanedConfig.leagueList = cleanedConfig.leagueList.split(',');
   }
-  if (config.numberOfDays) {
-    config.numberOfDays = parseInt(config.numberOfDays, 10);
+  if (cleanedConfig.numberOfDays) {
+    cleanedConfig.numberOfDays = parseInt(cleanedConfig.numberOfDays, 10);
+    if (Number.isNaN(cleanedConfig.numberOfDays)) delete cleanedConfig.numberOfDays;
   }
-  const slingProps = { ...config, ...defultProps };
+  const slingProps = { ...defultProps, ...cleanedConfig };
   const container = createTag('div', { id: 'gmfinder-app', 'data-sling-props': JSON.stringify(slingProps) });
   block.append(container);
 }
