6a22214853c81ef6da5225ca5449e4f04ca7a140
Evan Roper <eroper@adobe.com>
2025-05-22 08:17:43 -0600

    optimizing

--- aemedge/scripts/scripts.js
+++ aemedge/scripts/scripts.js
@@ -587,15 +587,8 @@ export function decorateExtImage() {
       const picture = document.createElement('picture');
       const img = document.createElement('img');
       img.classList.add('svg');
-
-      // Use the title attribute from Dynamic Media images as alt text if available
-      if (a.title) {
-        img.alt = a.title;
-      } else {
-        // Fallback to a default alt text if title is not available
-        img.alt = 'Sling TV image';
-      }
-
+      // if the link title to an external image was authored, assign as alt text, else use a default
+      img.alt = a.title || 'Sling TV image';
       // making assumption it is not LCP
       img.loading = 'lazy';
       img.src = extImageSrc;
