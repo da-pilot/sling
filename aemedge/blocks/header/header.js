279878d1d1bed9c9cc945043eee7cd3c68534cdc
Ritwik Srivastava <ritwik@Ritwiks-MacBook-Pro-2.local>
2025-06-11 11:06:16 +0530

    move default header to /aemedge for consistency

--- aemedge/blocks/header/header.js
+++ aemedge/blocks/header/header.js
@@ -127,7 +127,7 @@ function showsubmenu(nav, backbutton) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/aemedge/nav';
   const fragment = await loadFragment(navPath);
   // decorate nav DOM
   block.textContent = '';
